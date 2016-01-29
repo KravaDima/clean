53de83b4e5aa6ef6b7c473a71fd15ba2b4d9679d
KravaDima <Krava_Dima@mail.ru>
2016-01-29 15:13:47 +0200

    add new alert for form

--- pablic/js/form.js
+++ pablic/js/form.js
@@ -2,6 +2,7 @@ function checkFoo(){
 	var f = document.forms.order;
 	var elm = f.elements;
 	var isEmpty = false;
+	var isChecked = false;
 	for(var i=0; i<elm.length; i++){
 		if(elm[i].type == "text"){
 			if(elm[i].value == ""){
@@ -10,10 +11,16 @@ function checkFoo(){
 			} else {
 				elm[i].style.borderColor = "";
 			}
+		} else if(elm[i].type == "radio"){
+			if (elm[i].checked) {
+				isChecked = true;
+			}
 		}
 	}
 	if(isEmpty  || !(/\+380[0-9]{9}/.test(elm.tel.value))){
 		alert("Заполните правильно все поля!");
+	} else if(!isChecked){
+		alert("Выберите \"Тип уборки\"!");
 	} else {
 		f.submit();
 	}
